17629b9b31eeb3d0512ef81a8cd79664892468d9
Javierss <jasevill@pa.uc3m.es>
2024-07-31 14:06:04 +0200

    Move hid indicators

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -10,7 +10,7 @@
 // #include "widgets/bongo_cat.h"
 #include "widgets/layer_status.h"
 #include "widgets/output_status.h"
-// #include "widgets/hid_indicators.h"
+#include "widgets/hid_indicators.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -21,9 +21,9 @@ static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
 static struct zmk_widget_modifiers modifiers_widget;
 // static struct zmk_widget_bongo_cat bongo_cat_widget;
 
-// #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-// static struct zmk_widget_hid_indicators hid_indicators_widget;
-// #endif
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+static struct zmk_widget_hid_indicators hid_indicators_widget;
+#endif
 
 lv_style_t global_style;
 
@@ -48,10 +48,10 @@ lv_obj_t *zmk_display_status_screen()
     zmk_widget_modifiers_init(&modifiers_widget, screen);
     lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
-    // #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-    //     zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
-    //     lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
-    // #endif
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_BOTTOM_RIGHT, 0, 0);
+#endif
 
     // zmk_widget_layer_status_init(&layer_status_widget, screen);
     // lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
